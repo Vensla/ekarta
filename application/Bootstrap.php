7ad163892c2b5f7cf66600b97875ad3447c46003
SlavenGit <slaven91@gmail.com>
2013-12-07 18:10:40 +0100

    Ubacena forma za narucivanje.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,6 +9,9 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         
         $routePocetna = new Zend_Controller_Router_Route_Static('naslovna', array('controller' => 'Index', 'action' => 'index'));
         $router->addRoute('naslovna', $routePocetna);
+        
+        $routeNaruci = new Zend_Controller_Router_Route_Static('naruci/:b', array('controller' => 'Index', 'action' => 'naruci','b'=>''),array('b'=>'[0-9]{1,2}'));
+        $router->addRoute('naruci', $routeNaruci);
     }
 
 }
